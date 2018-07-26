be5d319a3098a282e32636b1074639bd7f342362
sebug <sebastian.gfeller@gmail.com>
2018-07-26 20:24:48 +0200

    Using static files for the moment

--- MailPolling/MailPolling/Startup.cs
+++ MailPolling/MailPolling/Startup.cs
@@ -25,10 +25,8 @@ namespace MailPolling
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
